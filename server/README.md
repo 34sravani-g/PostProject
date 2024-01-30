0ab29c158f69d3812c6d50ec38ca0f0c8c42fc22
sravani <gurramsravani664@gmail.com>
2024-01-30 23:34:57 +0530

    Initial commit

---
+++ server/README.md
@@ -0,0 +1,5 @@
+# Node.js Express & MongoDB: CRUD Rest APIs
+### Run
+```
+node server.js
+```
---
+++ server/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://gurramsravani19:9f3TmZAkAKQwhuFG@cluster0.zvcobgv.mongodb.net/?retryWrites=true&w=majority"
+};
